304d66d2b2a83365d62a7f1a3fa96afef7c7351d
DhandapaniSakthivel <98797379+DhandapaniSakthivel@users.noreply.github.com>
2024-11-23 19:21:10 +0530

    added address model and modified logic

---
+++ src/main/java/com/root/root/model/Member.java
@@ -0,0 +1,94 @@
+package com.root.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    String memberNumber;
+    Integer age;
+    String city;
+    String gender;
+    Integer noOfHOMPolicies;
+    Integer noOfRPIPolicies;
+    Integer noOfRENPolicies;
+
+    Address address;
+
+    public Member(String memberNumber, Integer age, String city, String gender, Integer noOfHOMPolicies, Integer noOfRPIPolicies, Integer noOfRENPolicies, Address address) {
+        this.memberNumber = memberNumber;
+        this.age = age;
+        this.city = city;
+        this.gender = gender;
+        this.noOfHOMPolicies = noOfHOMPolicies;
+        this.noOfRPIPolicies = noOfRPIPolicies;
+        this.noOfRENPolicies = noOfRENPolicies;
+        this.address = address;
+    }
+
+    public String getMemberNumber() {
+        return memberNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getNoOfHOMPolicies() {
+        return noOfHOMPolicies;
+    }
+
+    public Integer getNoOfRPIPolicies() {
+        return noOfRPIPolicies;
+    }
+
+    public Integer getNoOfRENPolicies() {
+        return noOfRENPolicies;
+    }
+
+    public void setMemberNumber(String memberNumber) {
+        this.memberNumber = memberNumber;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setNoOfHOMPolicies(Integer noOfHOMPolicies) {
+        this.noOfHOMPolicies = noOfHOMPolicies;
+    }
+
+    public void setNoOfRPIPolicies(Integer noOfRPIPolicies) {
+        this.noOfRPIPolicies = noOfRPIPolicies;
+    }
+
+    public void setNoOfRENPolicies(Integer noOfRENPolicies) {
+        this.noOfRENPolicies = noOfRENPolicies;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
